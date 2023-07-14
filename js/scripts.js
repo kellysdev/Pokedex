c64c189a76b55fa9ba09d74750d1cb954a23bb7f
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-07-14 15:05:50 -0700

    add first 9 pokemon from pokemon.com pokedex to data set
    bulbasaur, ivysaur, venusaur, charmander, charmeleon, charizard, squirtle, wartortle, and blastoise were added.  Each with the keys: name as a string, height as a float in feet, and types as an array of strings.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,47 @@
-let pokemonList = []
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 2.3,
+        types: ['grass', 'poinson']
+    },
+    {
+        name: 'Ivysaur',
+        height: 3.25,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Venusaur',
+        height: 6.58,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 2.0,
+        types: ['fire']
+    },
+    {
+        name: 'Charmeleon',
+        height: 3.58,
+        types: ['fire']
+    },
+    {
+        name: 'Charizard',
+        height:5.58,
+        types: ['fire', 'flying']
+    },
+    {
+        name: 'Squirtle',
+        height: 1.67,
+        types: ['water']
+    },
+    {
+        name: 'Wartortle',
+        height: 3.25,
+        types: ['water']
+    },
+    {
+        name: 'Blastoise',
+        height: 5.25,
+        types: ['water']
+    }
+]
