8c9a36962c6c09c2155ead8e6b0a78accce4dbf6
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-07-21 16:46:30 -0700

    Exercise 1.5 Task Part 2
    Surround the original pokemonList array in an IIFE, pokemonRepository and adjust the forEach loop to act on it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,65 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 2.3,
-        types: ['grass', 'poinson']
-    },
-    {
-        name: 'Ivysaur',
-        height: 3.25,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Venusaur',
-        height: 6.58,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 2.0,
-        types: ['fire']
-    },
-    {
-        name: 'Charmeleon',
-        height: 3.58,
-        types: ['fire']
-    },
-    {
-        name: 'Charizard',
-        height:5.58,
-        types: ['fire', 'flying']
-    },
-    {
-        name: 'Squirtle',
-        height: 1.67,
-        types: ['water']
-    },
-    {
-        name: 'Wartortle',
-        height: 3.25,
-        types: ['water']
-    },
-    {
-        name: 'Blastoise',
-        height: 5.25,
-        types: ['water']
+let pokemonRepository = (function(){
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 2.3,
+            types: ['grass', 'poinson']
+        },
+        {
+            name: 'Ivysaur',
+            height: 3.25,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Venusaur',
+            height: 6.58,
+            types: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 2.0,
+            types: ['fire']
+        },
+        {
+            name: 'Charmeleon',
+            height: 3.58,
+            types: ['fire']
+        },
+        {
+            name: 'Charizard',
+            height:5.58,
+            types: ['fire', 'flying']
+        },
+        {
+            name: 'Squirtle',
+            height: 1.67,
+            types: ['water']
+        },
+        {
+            name: 'Wartortle',
+            height: 3.25,
+            types: ['water']
+        },
+        {
+            name: 'Blastoise',
+            height: 5.25,
+            types: ['water']
+        }
+    ];
+
+    function getAll() {
+        return pokemonList;
     }
-]
+
+    function add(item) {
+        pokemonList.add(item)
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
 
 function myLoopFunction(pokemon) {
     document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ' ft)');
@@ -54,4 +69,4 @@ function myLoopFunction(pokemon) {
         document.write('</p>')
     }
 }
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction);
