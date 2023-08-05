fefbd0732bdae2334f0cfc92f1ff366241e33b12
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-08-05 15:57:14 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,7 @@ let pokemonRepository = (function(){
     function addListItem(pokemon) {
         let pokemonListVar = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
-        listItem.classList.add('list-group-item')
+        listItem.classList.add('list-group-item');
         let button = document.createElement('button');
         button.innerText = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
         button.classList.add('button');
