6c5b119caabf8ee1da49122cf1ce3041529e7872
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-08-15 17:59:24 -0700

    install ESLint, rename ESLint file, change to double quotes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,17 @@
 let pokemonRepository = (function(){
     let repository = [];
-    let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
-    let modalContainer = document.querySelector('#exampleModal');
+    let apiURL = "https://pokeapi.co/api/v2/pokemon/?limit=151";
+    let modalContainer = document.querySelector("#exampleModal");
 
     function getAll() {
         return repository;
     }
 
     function add(item) {
-        if (typeof item === 'object', Object.keys(item).includes('name', 'height', 'types'))  {
+        if (typeof item === "object", Object.keys(item).includes("name", "height", "types"))  {
             repository.push(item)
         }else {
-            alert('Verify the Pokemon information.')
+            alert("Verify the Pokemon information.")
         }
     }
 
@@ -51,25 +51,25 @@ let pokemonRepository = (function(){
     //creates buttons for each pokemon that displays their name
     function addListItem(pokemon) {
     
-        let pokemonList = document.querySelector('.pokemon-list');
-        let listItem = document.createElement('li');
-        listItem.classList.add('list-group-item');
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listItem = document.createElement("li");
+        listItem.classList.add("list-group-item");
         
-        let button = document.createElement('button');
-        // I can't get the sprites to appear in the buttons:
-        // button.innerHTML = pokemon.imageUrl +  '<br>' + pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+        let button = document.createElement("button");
+        // I can"t get the sprites to appear in the buttons:
+        // button.innerHTML = pokemon.imageUrl +  "<br>" + pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
         button.innerText = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
-        button.classList.add('button');
-        button.classList.add('btn');
-        button.classList.add('data-toggle');
-        button.classList.add('data-target');
-        button.setAttribute('data-bs-toggle', 'modal');
-        button.setAttribute('data-bs-target', '#exampleModal');
+        button.classList.add("button");
+        button.classList.add("btn");
+        button.classList.add("data-toggle");
+        button.classList.add("data-target");
+        button.setAttribute("data-bs-toggle", "modal");
+        button.setAttribute("data-bs-target", "#exampleModal");
         
         listItem.appendChild(button);
         pokemonList.appendChild(listItem);
 
-        button.addEventListener('click', function() {
+        button.addEventListener("click", function() {
             loadDetails(pokemon).then(function () {
                 populateModal(pokemon);
             })
@@ -77,7 +77,7 @@ let pokemonRepository = (function(){
     }
 
     //search:
-    let searchBar = document.querySelector('#form1');
+    let searchBar = document.querySelector("#form1");
     searchBar.addEventListener("input", (event) => {
         let searchString = event.target.value;
         let filteredList = repository.filter((item) => {
@@ -91,20 +91,20 @@ let pokemonRepository = (function(){
     
     //create modal:
     function populateModal(pokemon) {
-        let modalBody = $('.modal-body');
-        let modalTitle = $('.modal-title');
+        let modalBody = $(".modal-body");
+        let modalTitle = $(".modal-title");
 
         //clear existing content in the modal:
         modalTitle.empty();
         modalBody.empty();
 
-        let imageElement = $('<img class="modal-img" style="width:50%">')
-        imageElement.attr('src', pokemon.imageUrl);
-        let nameElement = $('<h1>' + pokemon.name[0].toUpperCase() + pokemon.name.slice(1) + '</h1>');
-        let idElement = $('<p>' + 'No.' + pokemon.id + '</p>');
-        let heightElement = $('<p>' + 'Height: ' + pokemon.height + ' meters' + '</p>');
-        let typesElement = $('<p>' + 'Types: ' + pokemon.types + '</p>');
-        let abilitiesElement = $('<p>' + 'Abilities: ' + pokemon.abilities  + '</p>');
+        let imageElement = $("<img class='modal-img' style='width:50%'>")
+        imageElement.attr("src", pokemon.imageUrl);
+        let nameElement = $("<h1>" + pokemon.name[0].toUpperCase() + pokemon.name.slice(1) + "</h1>");
+        let idElement = $("<p>" + "No." + pokemon.id + "</p>");
+        let heightElement = $("<p>" + "Height: " + pokemon.height + " meters" + "</p>");
+        let typesElement = $("<p>" + "Types: " + pokemon.types + "</p>");
+        let abilitiesElement = $("<p>" + "Abilities: " + pokemon.abilities  + "</p>");
 
         modalTitle.append(nameElement);
         modalBody.append(imageElement);
